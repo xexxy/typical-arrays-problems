e840524c5aec262226e60731b342320b6f6a90d4
xexxy <38164075+xexxy@users.noreply.github.com>
2020-10-02 17:22:56 +0300

    feat: add main functionality

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if ( Array.isArray(array) && array.length != 0) {
+        return Math.min(...array);
+    } else {
+        return 0;
+    }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if ( Array.isArray(array) && array.length != 0) {
+        let maxNumber = array[0];
+        array.forEach(element => {
+            if (maxNumber < element) {
+                maxNumber = element;
+            }
+        });
+        return maxNumber;
+    } else {
+        return 0;
+    }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if ( Array.isArray(array) && array.length != 0) {
+        let sum = array.reduce((accumulator, current) => accumulator + current);
+        return sum / array.length;
+    } else {
+        return 0;
+    }
 }
